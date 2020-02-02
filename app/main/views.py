549c6cb00ca18ea6e023a94755d30def3f1e07f6
Hannes Kimara <hanneskimara@gmail.com>
2020-02-03 01:17:19 +0300

    Implement routing for articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import main
 from ..request import get_sources, process_sources
 
@@ -9,3 +9,11 @@ def index():
     data = process_sources(news_sources)
     
     return render_template('index.html', sources = data)
+
+@main.route("/articles/")
+def red_articles():
+    return redirect(url_for('main.index'))
+
+@main.route("/articles/<source_id>")
+def articles(source_id = None):
+    return render_template("articles.html", source_id = source_id)
